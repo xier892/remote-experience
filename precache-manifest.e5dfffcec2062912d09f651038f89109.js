019383b52e9d521e51fece120bd89ab63dc47d3d
Ricky Xie <xier892@newschool.edu>
2020-10-22 14:19:04 -0400

    Updates

---
+++ precache-manifest.e5dfffcec2062912d09f651038f89109.js
@@ -0,0 +1,326 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d58d84979f41aa308a177ccaa878e9e",
+    "url": "/remote-experience/index.html"
+  },
+  {
+    "revision": "8c3fdd788dbd39be30cc",
+    "url": "/remote-experience/static/css/main.e6d746e1.chunk.css"
+  },
+  {
+    "revision": "ce6959ba0f666f24dd1a",
+    "url": "/remote-experience/static/js/2.a7104c0f.chunk.js"
+  },
+  {
+    "revision": "07e6b9a4d0f72bd616bee2a44adf91a9",
+    "url": "/remote-experience/static/js/2.a7104c0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c3fdd788dbd39be30cc",
+    "url": "/remote-experience/static/js/main.7f7470bd.chunk.js"
+  },
+  {
+    "revision": "ae2d3174d6ce6547f02d",
+    "url": "/remote-experience/static/js/runtime-main.7d728162.js"
+  },
+  {
+    "revision": "4f61f23f3051181e321b61b4d205639a",
+    "url": "/remote-experience/static/media/Alert 1.4f61f23f.m4a"
+  },
+  {
+    "revision": "0f43c6e32e964a6cf078c996b5e996f7",
+    "url": "/remote-experience/static/media/Alert 2.0f43c6e3.m4a"
+  },
+  {
+    "revision": "a410e8d7937adb6aa77fd281ceb7f564",
+    "url": "/remote-experience/static/media/Alert 3.a410e8d7.m4a"
+  },
+  {
+    "revision": "21a11b92433c8a76d028843498d74708",
+    "url": "/remote-experience/static/media/Alert 4.21a11b92.m4a"
+  },
+  {
+    "revision": "929617c2a72bbf1443a2bc90493e8bd4",
+    "url": "/remote-experience/static/media/Alert 5.929617c2.m4a"
+  },
+  {
+    "revision": "98e416db37f3bf33076e47df8d963cb5",
+    "url": "/remote-experience/static/media/Button 1.98e416db.m4a"
+  },
+  {
+    "revision": "585ccefd1566451f27b6e4356de1b0d6",
+    "url": "/remote-experience/static/media/Button 2.585ccefd.m4a"
+  },
+  {
+    "revision": "e149bd38611dff5bce1804f6fbb2bd2d",
+    "url": "/remote-experience/static/media/Button 3.e149bd38.m4a"
+  },
+  {
+    "revision": "d6fafc1365311501dcfb60cdcbdf63e8",
+    "url": "/remote-experience/static/media/Button 4.d6fafc13.m4a"
+  },
+  {
+    "revision": "18cc20013f903d28069362ee0029c0b9",
+    "url": "/remote-experience/static/media/Button 5.18cc2001.m4a"
+  },
+  {
+    "revision": "29206fc12accc7036e3c39ba88273dd7",
+    "url": "/remote-experience/static/media/Button 6.29206fc1.m4a"
+  },
+  {
+    "revision": "03b1300fa37b529f50a51d73da1213c8",
+    "url": "/remote-experience/static/media/Button 7.03b1300f.m4a"
+  },
+  {
+    "revision": "e95d83c45040dedbae9b44cdf2c9b611",
+    "url": "/remote-experience/static/media/Cancel 1.e95d83c4.m4a"
+  },
+  {
+    "revision": "874aca67db10803d3801deded12bfec9",
+    "url": "/remote-experience/static/media/Cancel 2.874aca67.m4a"
+  },
+  {
+    "revision": "83eab647d6931beb09f67df597cccae8",
+    "url": "/remote-experience/static/media/Collapse.83eab647.m4a"
+  },
+  {
+    "revision": "4c8cd6ea9fc9310b0b0aa6391f19be82",
+    "url": "/remote-experience/static/media/Error 1.4c8cd6ea.m4a"
+  },
+  {
+    "revision": "62a31a9d61b240de840040d98afbe468",
+    "url": "/remote-experience/static/media/Error 2.62a31a9d.m4a"
+  },
+  {
+    "revision": "6839aea352cef88ab82eeccc4637ad29",
+    "url": "/remote-experience/static/media/Error 3.6839aea3.m4a"
+  },
+  {
+    "revision": "8422035747ccb5c643398721357ab886",
+    "url": "/remote-experience/static/media/Error 4.84220357.m4a"
+  },
+  {
+    "revision": "11d5a413c821861e8343adfa614faefd",
+    "url": "/remote-experience/static/media/Error 5.11d5a413.m4a"
+  },
+  {
+    "revision": "798133cac449d3d79a0a045cc9a6fbe7",
+    "url": "/remote-experience/static/media/Expand.798133ca.m4a"
+  },
+  {
+    "revision": "a77aa55310d6afb6d74105d17ca35bd0",
+    "url": "/remote-experience/static/media/Notification 1.a77aa553.m4a"
+  },
+  {
+    "revision": "305874b90c14e41514ff147c4b9dd779",
+    "url": "/remote-experience/static/media/Notification 2.305874b9.m4a"
+  },
+  {
+    "revision": "ebd261b20220c89098c2974dc97b6cad",
+    "url": "/remote-experience/static/media/Notification 3.ebd261b2.m4a"
+  },
+  {
+    "revision": "3d47f6d4c78646ec528af44a26de4799",
+    "url": "/remote-experience/static/media/Notification 4.3d47f6d4.m4a"
+  },
+  {
+    "revision": "ef6285c0f6a32edc874bbdbc1356924f",
+    "url": "/remote-experience/static/media/Notification 5.ef6285c0.m4a"
+  },
+  {
+    "revision": "c8b64035f3e8a6ac860a583b6f59ac29",
+    "url": "/remote-experience/static/media/Notification 6.c8b64035.m4a"
+  },
+  {
+    "revision": "1639da07a3d60fcd9b59bd62eefe0846",
+    "url": "/remote-experience/static/media/Notification 7.1639da07.m4a"
+  },
+  {
+    "revision": "308e739f1d83b1a8cd2b691da414e7fd",
+    "url": "/remote-experience/static/media/Notification 8.308e739f.m4a"
+  },
+  {
+    "revision": "ca5a1c7bbee2292e047f7cc836e16a36",
+    "url": "/remote-experience/static/media/Notification 9.ca5a1c7b.m4a"
+  },
+  {
+    "revision": "4f3c2e43b06aa944965d0c3276aafed2",
+    "url": "/remote-experience/static/media/Success 1.4f3c2e43.m4a"
+  },
+  {
+    "revision": "03d319506f27398185cb8a345f176fb6",
+    "url": "/remote-experience/static/media/Success 2.03d31950.m4a"
+  },
+  {
+    "revision": "2bf567d31fe38c17c1a89143454fd175",
+    "url": "/remote-experience/static/media/Success 3.2bf567d3.m4a"
+  },
+  {
+    "revision": "c217bc9a3088ea2c81515bbe46dcefa8",
+    "url": "/remote-experience/static/media/Tab 1.c217bc9a.m4a"
+  },
+  {
+    "revision": "46782a7e7de2267e5210f6e31cc0ba25",
+    "url": "/remote-experience/static/media/Tab 2.46782a7e.m4a"
+  },
+  {
+    "revision": "e42fbe351848c6d90f12eb294bb060cc",
+    "url": "/remote-experience/static/media/Tab 3.e42fbe35.m4a"
+  },
+  {
+    "revision": "64442a05dab61cb415a913f28683c730",
+    "url": "/remote-experience/static/media/arrow-left.64442a05.svg"
+  },
+  {
+    "revision": "fa986b3d2d9ee0cf1d0a9648fe3f0c68",
+    "url": "/remote-experience/static/media/arrow-right.fa986b3d.svg"
+  },
+  {
+    "revision": "dbb828829968204f2345c4455334be1c",
+    "url": "/remote-experience/static/media/crying.dbb82882.mp3"
+  },
+  {
+    "revision": "f2eaf4df3f7caeaeaf1e05bc6e0e71f1",
+    "url": "/remote-experience/static/media/crying.f2eaf4df.wav"
+  },
+  {
+    "revision": "91f550b0bc7aceafdd5598d3a538062c",
+    "url": "/remote-experience/static/media/duck.91f550b0.svg"
+  },
+  {
+    "revision": "fb65291fbef2cfc1954c7dabe9c2a300",
+    "url": "/remote-experience/static/media/enableSharing.fb65291f.png"
+  },
+  {
+    "revision": "a484729e19cbb1090b916d8a8c2fa85d",
+    "url": "/remote-experience/static/media/inaccessible-mouse-pointer.a484729e.svg"
+  },
+  {
+    "revision": "d951489803e1e67db57bc445abddd77a",
+    "url": "/remote-experience/static/media/jazz-ambient.d9514898.mp3"
+  },
+  {
+    "revision": "fb1b5447f9233087956c3b41a9115d43",
+    "url": "/remote-experience/static/media/location.fb1b5447.svg"
+  },
+  {
+    "revision": "4c608ead0af1b0ad0a21408c89e4b08a",
+    "url": "/remote-experience/static/media/marlett.4c608ead.svg"
+  },
+  {
+    "revision": "71c994d68b063c3480dd8c4b94444845",
+    "url": "/remote-experience/static/media/marlett.71c994d6.eot"
+  },
+  {
+    "revision": "77a808f9f49cfe17f3671c62fae19769",
+    "url": "/remote-experience/static/media/marlett.77a808f9.woff2"
+  },
+  {
+    "revision": "a67cf418d56f1008f064cef6ad7ffbda",
+    "url": "/remote-experience/static/media/marlett.a67cf418.woff"
+  },
+  {
+    "revision": "ffe58ece786e75057f936f7bfe3e40eb",
+    "url": "/remote-experience/static/media/marlett.ffe58ece.ttf"
+  },
+  {
+    "revision": "4e8c4def5ab62d9091773be1a5584ab7",
+    "url": "/remote-experience/static/media/microphone-slash.4e8c4def.svg"
+  },
+  {
+    "revision": "2e49559f24bfc033f7a66d1a369d9858",
+    "url": "/remote-experience/static/media/neosphere-ambient.2e49559f.wav"
+  },
+  {
+    "revision": "aa15b53c4dd6457136d23ee98b3a6716",
+    "url": "/remote-experience/static/media/neosphere-ambient.aa15b53c.mp3"
+  },
+  {
+    "revision": "a59ec3b8544bf499b9ecbb32232bc38d",
+    "url": "/remote-experience/static/media/open.a59ec3b8.wav"
+  },
+  {
+    "revision": "d87e265cb69ebd938dec8317fa4a9217",
+    "url": "/remote-experience/static/media/open.d87e265c.mp3"
+  },
+  {
+    "revision": "c5f766c03b2bc751f8e8fa125162131b",
+    "url": "/remote-experience/static/media/plane.c5f766c0.svg"
+  },
+  {
+    "revision": "0f739011f992a553eca6341d2488234c",
+    "url": "/remote-experience/static/media/poppins-latin-400.0f739011.woff2"
+  },
+  {
+    "revision": "70131db52482405c208927cc11b2322c",
+    "url": "/remote-experience/static/media/poppins-latin-400.70131db5.woff"
+  },
+  {
+    "revision": "e984294171acbaf00003af3915621e46",
+    "url": "/remote-experience/static/media/poppins-latin-500.e9842941.woff2"
+  },
+  {
+    "revision": "eaab31243d14e5e02cfb6ffabb8f1bb3",
+    "url": "/remote-experience/static/media/poppins-latin-500.eaab3124.woff"
+  },
+  {
+    "revision": "178ffc7be81fd0fa87c56ab2019e6a88",
+    "url": "/remote-experience/static/media/poppins-latin-600.178ffc7b.woff"
+  },
+  {
+    "revision": "b54e7266f2529c2aa7ac3cd8e2247381",
+    "url": "/remote-experience/static/media/poppins-latin-600.b54e7266.woff2"
+  },
+  {
+    "revision": "4677deb17bdaa503f8c09f81b013af31",
+    "url": "/remote-experience/static/media/prison.4677deb1.mp3"
+  },
+  {
+    "revision": "621119eb4a528a2d1e077809cabfb281",
+    "url": "/remote-experience/static/media/prison.621119eb.wav"
+  },
+  {
+    "revision": "4d6bd8113098f9fd9736da00eefc17ff",
+    "url": "/remote-experience/static/media/static.4d6bd811.mp3"
+  },
+  {
+    "revision": "42c811021b1e9881efda328de9c181cb",
+    "url": "/remote-experience/static/media/tv-ambient.42c81102.mp3"
+  },
+  {
+    "revision": "ad2ab7e7cf9c01763f2e1dfe9f8a4f07",
+    "url": "/remote-experience/static/media/tv-ambient.ad2ab7e7.wav"
+  },
+  {
+    "revision": "1688d7abcd8d847cfe1c7796bf0aff27",
+    "url": "/remote-experience/static/media/ufo-ambient.1688d7ab.mp3"
+  },
+  {
+    "revision": "7c42e320de32aacb6819bf0a29e43ff1",
+    "url": "/remote-experience/static/media/ufo-ambient.7c42e320.wav"
+  },
+  {
+    "revision": "e6be472ea2046e0917d0b94e2e4ec42b",
+    "url": "/remote-experience/static/media/waiting-audio.e6be472e.svg"
+  },
+  {
+    "revision": "804b523ac6e52afcd2c05885d7f09f50",
+    "url": "/remote-experience/static/media/waiting-location.804b523a.svg"
+  },
+  {
+    "revision": "d6b0653ab0c3a3c07f54f9812159d978",
+    "url": "/remote-experience/static/media/waiting-video.d6b0653a.svg"
+  },
+  {
+    "revision": "0752b101b73065c3ae0e47187cbb5db2",
+    "url": "/remote-experience/static/media/zoombell.0752b101.ogg"
+  },
+  {
+    "revision": "24224a59fffab1f90af9e37741385b72",
+    "url": "/remote-experience/static/media/zoombell.24224a59.mp3"
+  },
+  {
+    "revision": "ef82d6b6ba00d9c9a908eb0ae4208321",
+    "url": "/remote-experience/static/media/zoombell.ef82d6b6.wav"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/remote-experience/precache-manifest.39ea9f3a23a022cd2212b5143440e441.js"
+  "/remote-experience/precache-manifest.e5dfffcec2062912d09f651038f89109.js"
 );
 
 self.addEventListener('message', (event) => {
